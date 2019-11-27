9d1120f0d818b2569bd6c83e8048436eea828d8c
Mathew Heard <splitice@users.noreply.github.com>
2019-11-27 21:23:53 +1100

    linux 5.4 support

--- src/xt_NOCREATE.c
+++ src/xt_NOCREATE.c
@@ -21,10 +21,7 @@ nocreate_tg(struct sk_buff *skb, const struct xt_action_param *par)
 	struct nf_conn * tmpl = nf_ct_get(skb, &ctinfo);
 	if (tmpl == NULL) {
 		atomic_inc(&info->ct->ct_general.use);
-		skb->nfct = &info->ct->ct_general;
-		skb->nfctinfo = IP_CT_NEW;
-		goto end;
-	} else if(unlikely(nf_ct_is_untracked(tmpl))) {
+		nf_ct_set(skb, info->ct, IP_CT_NEW);
 		goto end;
 	}
 
@@ -41,11 +38,14 @@ static int nocreate_chk(const struct xt_tgchk_param *par)
 	struct xt_nocreate_target_info *info = par->targinfo;
 	struct nf_conn *ct;
 	struct nf_conntrack_zone zone;
-	int ret = 0;
-	
-	ret = nf_ct_l3proto_try_module_get(par->family);
-    if (ret < 0)
-        goto err1;
+	int ret;	
+
+	ret = nf_ct_netns_get(par->net, par->family);
+	if (ret < 0){
+		pr_info_ratelimited("cannot load conntrack support for proto=%u\n",
+				    par->family);
+        	goto err1;
+	}
 	
 	memset(&zone, 0, sizeof(zone));
 	zone.dir = NF_CT_DEFAULT_ZONE_DIR;
@@ -65,7 +65,7 @@ static int nocreate_chk(const struct xt_tgchk_param *par)
 err:
 	return ret;
 err1:
-	nf_ct_l3proto_module_put(par->family);
+//	nf_ct_l3proto_module_put(par->family);
 	return ret;
 }
 
@@ -73,9 +73,8 @@ static void xt_nocreate_tg_destroy(const struct xt_tgdtor_param *par,
 			     struct xt_nocreate_target_info *info)
 {
 	nf_ct_put(info->ct);
-	nf_ct_l3proto_module_put(par->family);
+	nf_ct_netns_put(par->net, par->family);
 }
-
 
 static void xt_nocreate_tg_destroy_v0(const struct xt_tgdtor_param *par)
 {
